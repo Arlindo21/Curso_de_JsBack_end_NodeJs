87543511f660d1e0781017f21eb7599b804bbd18
Arlindolima <Arlindolima307@gmail.com>
2024-12-19 21:16:23 -0300

    estudando com o livro

---
+++ Curso_Js_Back_end=LIvro/controlofuncao.js
@@ -0,0 +1,31 @@
+//usando apenas a condiçaõ if else
+let senha="123";
+if(senha==="123"){
+    console.log("Bem VIndo");
+}else{
+    console.log("senha errada");
+}
+//usando switch/case
+switch(senha){
+    case "12":
+    console.log("Bem VIndo");
+    break;
+    default:
+    console.log("senha errada");
+ break;
+}
+"bem vindo"
+////exemplo abaixo é para usar no console do navegador com o Alert
+let nome=[];
+nome="arlindo","Lima";
+switch(nome){
+    case "arlindo":
+        console.log("Bem Vindo Keny");
+       break;
+    case "Lima":
+        console.log("Bem Ynek21");
+        default:
+            console.log("COmando invalido");
+            break;
+
+}
---
+++ Curso_Js_Back_end=LIvro/loopFor.js
@@ -0,0 +1,12 @@
+const arr=[1,2,3,5,7,11];
+
+for (let i = 0,max = arr.length ; i < max; i++){
+    console.log(arr[i]);
+}
+//loop while
+let i = 3;
+const max = arr.length;
+while(i<max){
+    console.log(arr[i]);
+    i++
+}
