25e982cf8484a79eeb486280a3d8db849e7f8e9a
Arlindolima <Arlindolima307@gmail.com>
2025-01-21 12:35:12 -0300

    AluraJs

---
+++ Curso_Js_Back_end=ALura/js_Arrays/for_Classico.js
@@ -0,0 +1,5 @@
+const notas=[10,6.5,8,7,6];
+for(i=0; i< notas.length; i++){
+console.log(i, notas[i]);
+
+}
---
+++ Curso_Js_Back_end=ALura/js_Arrays/incremento.js
@@ -0,0 +1,10 @@
+let i = 5;
+console.log(i++); // Saída: 5 (retorna o valor atual de i)
+console.log(i++);   // Saída: 6 (i foi incrementado após a execução)
+console.log(i);
+
+let j = 10;
+console.log(j);
+
+console.log(++j); // Saída: 5 (retorna o valor atual de i)
+console.log(j);   // Saída: 6 (i foi incrementado após a execução)
